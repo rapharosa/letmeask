17f3e09df4bf651a2a11e28b8ada822bcd4166d2
Raphael <raphaelrodrigues93@gmail.com>
2022-03-24 20:12:35 -0300

    criando um botão contador com useState()

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/Button'
 function App() {
   return (
     <div>
-      <Button text = 'Botão 1' />
+      <Button />
       <Button />
       <Button />
       <Button />
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,15 @@
-type ButtonProps = {
-    text?: string;
-}
-export function Button (props: ButtonProps) {
+import { useState } from "react"
+
+export function Button () {
+    const [counter, setCounter] = useState(0)
+
+    function increment(){
+        setCounter(counter +1)
+        console.log(counter)
+    }
     return(
-        <button>{props.text || 'Default'}</button>
+        <button onClick={increment}>
+            {counter}
+        </button>
     )
 }
