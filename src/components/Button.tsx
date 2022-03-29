72af7781a103f563d09d5c2d2ace086184a6c15c
Raphael <raphaelrodrigues93@gmail.com>
2022-03-29 18:54:24 -0300

    estilização do Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {  ButtonHTMLAttributes  } from 'react';
 
+import '../styles/button.scss';
+
 type ButtonProps = ButtonHTMLAttributes <HTMLButtonElement>
 
 export function Button (props: ButtonProps) {
