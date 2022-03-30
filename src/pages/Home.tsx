cf3f5252bdf1023962fd8d368375e023baf8d979
Raphael <raphaelrodrigues93@gmail.com>
2022-03-29 22:29:15 -0300

    conectando as rotas **com erro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import { useHistory } from 'react-router-dom';
 
-import '../styles/auth.scss'
-import { Button } from '../components/Button'
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
 
 export function Home(){
+    const history = useHistory();
+
+    function navigateToNewRoom() {
+        history.push('/room/new')
+    }
+
     return(
         <div id='page-auth'>
             <aside>
@@ -16,7 +24,7 @@ export function Home(){
             <main className='main-content'>
                 <div>
                     <img src={logoImg} alt="Latmeask" />
-                    <button className='create-room'>
+                    <button onClick={navigateToNewRoom} className='create-room'>
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
